243b296197458cee11d50ae09edddf3c8ab6b0ba
yossydev <codeyy021@gmail.com>
2024-03-06 06:03:55 +0900

    feat: cors setting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use likes::{get_likes, like};
 mod likes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(like).service(get_likes))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header()
+            .supports_credentials()
+            .max_age(3600);
+        App::new().wrap(cors).service(like).service(get_likes)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
