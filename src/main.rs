442619fa7c5fdefacffb7547f06a7ac03e4fd947
yossydev <codeyy021@gmail.com>
2024-03-06 15:37:42 +0900

    feat: corsの設定

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-use actix_web::{App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http::header, App, HttpServer};
 use likes::{get_likes, like};
 mod likes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(like).service(get_likes))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:5173")
+                    .allowed_origin("http://localhost:8788")
+                    .allowed_origin("yossydev-blog.pages.dev")
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .supports_credentials()
+                    .max_age(3600),
+            )
+            .service(like)
+            .service(get_likes)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
